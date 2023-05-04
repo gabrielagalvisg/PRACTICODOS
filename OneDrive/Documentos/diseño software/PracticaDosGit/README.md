80e7df4fa418dacb835ff7a62e469e5c4011c461
gabrielagg <galvis.gabriela.5062@eam.edu.co>
2023-05-04 12:50:43 -0500

    se creo la estructura del proyecto

---
+++ OneDrive/Documentos/diseño software/PracticaDosGit/README.md
@@ -0,0 +1,8 @@
+# PRACTICA DOS 
+Este es un archivo creado con la idea de poner en practica nuestros conocimientos 
+
+##Pre - requisito
+*Java
+
+##Autora
+Gabriela
---
+++ OneDrive/Documentos/diseño software/PracticaDosGit/src/practicadosgit/PracticaDosGit.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package practicadosgit;
+
+/**
+ *
+ * @author Ggalv
+ */
+public class PracticaDosGit {
+
+    /**
+     * ## son subtitulo
+     * # titulo
+     * (* Viñeta
+     * 
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
